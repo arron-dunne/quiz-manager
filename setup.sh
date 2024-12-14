40738e5f0bcad84b619949ff71a20dd4decae588
arron-dunne <arrondunneps@gmail.com>
2024-12-14 14:55:25 +1100

    improved setup.sh

--- setup.sh
+++ setup.sh
@@ -3,11 +3,10 @@ echo 'setting up virtual environement...'
 python3.11 -m venv .venv
 source .venv/bin/activate
 echo 'installing dependencies...'
-pip install -q -r requirements.txt
+pip install --disable-pip-version-check -q -r requirements.txt
 flask init-db
 flask add-data-db
-echo "SECRET_KEY='dev'" > instance/config.py
+echo "SECRET_KEY='dev'" > ./instance/config.py
 echo 'secret key created in instance/config.py (update as necessary)'
-echo 'ready to start quiz-manager'
-echo "run 'source .venv/bin/activate' to start virtual environment"
-echo "then run 'flask run' to start application\n"
+echo '\nstarting quiz-manager...\n'
+flask run
